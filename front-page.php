3694c43a7288b1aad3a737fce692c558874cc9bc
twings07 <twingstan2503@gmail.com>
2022-03-18 15:12:25 -0400

    s8 Vendredi, Edward Twingstan, Fin des animations

--- front-page.php
+++ front-page.php
@@ -9,12 +9,18 @@
     <div class="animation__bloc">5</div>
 </section>
 
-    
+<div class="pagePrincipal">
+   <h1 class="pagePrincipal__titre"> Collège de Maisonneuve </h1>
+
+   </div>
+
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>   
   
    <?php endif ?>
    
+    
+
 </main>
 <?php get_footer() ?>
